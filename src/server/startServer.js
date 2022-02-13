46a6ffadbd45a9209bcf0dac4cba355dc756701e
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-13 15:43:55 -0400

    feat: :sparkles: create startServer file

---
+++ src/server/startServer.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server-express';
+import 'dotenv/config';
+
+export default function startSever({ typeDefs, resolvers }) {
+  const { PORT } = process.env;
+  const app = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: (req) => req.headers || '',
+  });
+
+  app.listen(PORT || 4000, () =>
+    console.log(`🔥 App Running on https://studio.apollographql.com/`)
+  );
+}
