3fb55efaf3b0eca1df8a60d3f6cb68d64ac54a4a
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-13 14:18:02 -0400

    feat: :wrench: config babel

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  ignore: ['*/.spec.ts', '*/.test.ts', './dist/*/'],
+};
