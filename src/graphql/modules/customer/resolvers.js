e038fc722719d552266415c17bb61518bd7a71b8
AdaoBJr <adaobjr@gmail.com>
2022-02-27 16:41:39 -0400

    feat: :sparkles: created hash id with uuidV4 on Customer Resolver

--- src/graphql/modules/customer/resolvers.js
+++ src/graphql/modules/customer/resolvers.js
@@ -1,4 +1,5 @@
 import Customer from '../../../repositories/mongodb/models/customer';
+import { v4 as uuidV4 } from 'uuid';
 
 export default {
   Query: {
@@ -6,7 +7,9 @@ export default {
   },
   Mutation: {
     createCustomer: async (_, { data }) => {
-      const customer = await Customer.create(data);
+      const id = uuidV4();
+      const customerData = { id, ...data };
+      const customer = await Customer.create(customerData);
       return { customer };
     },
   },
