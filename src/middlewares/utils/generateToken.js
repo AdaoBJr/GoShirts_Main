ae39defe7b75f1cc58dfa258f13842937a275934
AdaoBJr <adaobjr@gmail.com>
2022-02-28 14:19:34 -0400

    feat: ✨ created generateToken on utils

---
+++ src/middlewares/utils/generateToken.js
@@ -0,0 +1,13 @@
+import { sign } from 'jsonwebtoken';
+const { JWT_SECRET } = process.env;
+
+const jwtConfig = {
+  algorithm: 'HS256',
+  expiresIn: '1d',
+};
+
+const generateToken = ({ email }) => ({
+  token: sign({ data: email }, JWT_SECRET, jwtConfig),
+});
+
+export { generateToken };
