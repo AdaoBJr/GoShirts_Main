c21e164502371aba936510ac6fee1aed0d0881d2
AdaoBJr <adaobjr@gmail.com>
2022-03-01 17:51:03 -0400

    refactor: ♻ updated export on checkEmailExists

--- src/magicPlace/utils/checkEmailExists.js
+++ src/magicPlace/utils/checkEmailExists.js
@@ -3,4 +3,4 @@ import CustomerRepository from '../../repositories/mongodb/models/customer';
 const checkEmailExists = async ({ email }) =>
   !!(await CustomerRepository.findOne({ email }).exec());
 
-export { checkEmailExists };
+export default checkEmailExists;
