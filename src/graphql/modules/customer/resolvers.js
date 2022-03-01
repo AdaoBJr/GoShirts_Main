bde2049a8e2d0130dbeeeea59b3f0dfdd5cf87aa
AdaoBJr <adaobjr@gmail.com>
2022-03-01 16:20:23 -0400

    refactor: ♻ updated updateCustomer on Resolvers

--- src/graphql/modules/customer/resolvers.js
+++ src/graphql/modules/customer/resolvers.js
@@ -2,8 +2,8 @@ import { useCustomer } from '../../../magicPlace/talons/useCustomer';
 const {
   CustomerList,
   Customer,
-  UpdateCustomerById,
-  DeleteCustomerByEmail,
+  UpdateCustomer,
+  DeleteCustomer,
   CreateCustomer,
   SignInCustomer,
 } = useCustomer();
@@ -11,13 +11,12 @@ const {
 export default {
   Query: {
     customerList: async () => await CustomerList(),
-    customer: async (_, { email }) => await Customer({ email }),
+    customer: async (_, { id }) => await Customer({ id }),
   },
   Mutation: {
     createCustomer: async (_, { data }) => await CreateCustomer({ data }),
-    updateCustomerById: async (_, { hash: id, data }) =>
-      await UpdateCustomerById({ id, data }),
-    deleteCustomerByEmail: async (_, { email }) => await DeleteCustomerByEmail({ email }),
+    updateCustomer: async (_, args) => await UpdateCustomer({ args }),
+    deleteCustomer: async (_, { id }) => await DeleteCustomer({ id }),
     signInCustomer: async (_, { data }) => await SignInCustomer({ data }),
   },
 };
