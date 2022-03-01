dd6f45dbc7990622c887636dda6d06456e252175
AdaoBJr <adaobjr@gmail.com>
2022-03-01 11:35:59 -0400

    feat: :boom: added decoded jwt on checkAuthentication - completed

--- src/magicPlace/middlewares/checkAuthentication.js
+++ src/magicPlace/middlewares/checkAuthentication.js
@@ -1,31 +1,32 @@
 import { verify } from 'jsonwebtoken';
+import Customer from '../../repositories/mongodb/models/customer';
+
 const { JWT_SECRET } = process.env;
 
 const isAuth = async (resolve, parent, args, context, info) => {
   const authHeader = context.authorization;
-  const hasBearer = authHeader.includes('Bearer');
-  let token = '';
-
   if (!authHeader) throw new Error('Token is missing.');
+
+  let token = '';
+  const hasBearer = authHeader.includes('Bearer');
+
   if (hasBearer) [, token] = authHeader.split(' ');
   if (!hasBearer) token = authHeader;
 
-  try {
-    const decoded = verify(token, JWT_SECRET);
-    console.log(decoded);
-  } catch (error) {
-    throw new Error('Invalid Token!');
-  }
+  const { data: email } = verify(token, JWT_SECRET);
+  const userExists = await Customer.findOne({ email }).exec();
+  if (!userExists) throw new Error('Invalid token or unauthorized user.');
 
   return await resolve(parent, args, context, info);
 };
 
 const checkAuthentication = {
   Query: {
-    customerList: isAuth,
+    customerById: isAuth,
   },
   Mutation: {
     updateCustomerById: isAuth,
+    deleteCustomerById: isAuth,
   },
 };
 
