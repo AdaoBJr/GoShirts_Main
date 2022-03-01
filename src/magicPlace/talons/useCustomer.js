6e0b51bc7ed259435e2ee32cc6529451d2b446ed
AdaoBJr <adaobjr@gmail.com>
2022-03-01 16:21:14 -0400

    refactor: ♻ updated updateCustomer on useCustomer

--- src/magicPlace/talons/useCustomer.js
+++ src/magicPlace/talons/useCustomer.js
@@ -2,24 +2,24 @@ import CustomerRepository from '../../repositories/mongodb/models/customer';
 import { v4 as uuidV4 } from 'uuid';
 import { hash, compare } from 'bcrypt';
 import { generateToken } from '../utils/generateToken';
+import { checkEmailExists } from '../utils/checkEmailExists';
 
 const useCustomer = () => {
   const CustomerList = async () => await CustomerRepository.find();
 
-  const Customer = async ({ email }) =>
-    await CustomerRepository.findOne({ email }).exec();
+  const Customer = async ({ id }) => await CustomerRepository.findOne({ id }).exec();
 
-  const UpdateCustomerById = async ({ id, data }) => ({
+  const UpdateCustomer = async ({ args: { id, data } }) => ({
     customer: await CustomerRepository.findOneAndUpdate(id, data, { new: true }),
   });
 
-  const DeleteCustomerByEmail = async ({ email }) => ({
-    delete: !!(await CustomerRepository.findOneAndDelete(email)),
+  const DeleteCustomer = async ({ id }) => ({
+    delete: !!(await CustomerRepository.findOneAndDelete(id)),
   });
 
   const CreateCustomer = async ({ data }) => {
-    const user = await CustomerRepository.findOne({ email: data.email }).exec();
-    if (user) throw new Error('User already registered');
+    const user = await checkEmailExists({ email: data.email });
+    if (user) throw new Error('Email already registered');
 
     const id = uuidV4();
     const passwordHash = await hash(data.password, 8);
@@ -38,14 +38,14 @@ const useCustomer = () => {
     const passwordMatch = await compare(password, user.password);
     if (!passwordMatch) throw new Error('Email or password incorrect!');
 
-    return generateToken({ email });
+    return generateToken({ id: user.id });
   };
 
   return {
     CustomerList,
     Customer,
-    UpdateCustomerById,
-    DeleteCustomerByEmail,
+    UpdateCustomer,
+    DeleteCustomer,
     CreateCustomer,
     SignInCustomer,
   };
