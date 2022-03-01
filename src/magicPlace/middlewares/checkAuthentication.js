64adf463ef10944306f1fac22d0f29989ec5cfd5
AdaoBJr <adaobjr@gmail.com>
2022-02-28 23:24:34 -0400

    feat: :construction: adding decoded jwt on checkAuthentication

--- src/magicPlace/middlewares/checkAuthentication.js
+++ src/magicPlace/middlewares/checkAuthentication.js
@@ -1,5 +1,21 @@
+import { verify } from 'jsonwebtoken';
+const { JWT_SECRET } = process.env;
+
 const isAuth = async (resolve, parent, args, context, info) => {
-  if (!context.authorization) throw new Error('Not authorized');
+  const authHeader = context.authorization;
+  const hasBearer = authHeader.includes('Bearer');
+  let token = '';
+
+  if (!authHeader) throw new Error('Token is missing.');
+  if (hasBearer) [, token] = authHeader.split(' ');
+  if (!hasBearer) token = authHeader;
+
+  try {
+    const decoded = verify(token, JWT_SECRET);
+    console.log(decoded);
+  } catch (error) {
+    throw new Error('Invalid Token!');
+  }
 
   return await resolve(parent, args, context, info);
 };
