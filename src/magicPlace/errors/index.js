059bede9f0d9daa10b93f269a2b1085bb3a17deb
AdaoBJr <adaobjr@gmail.com>
2022-03-02 09:14:54 -0400

    feat: :sparkles: added new error on ApiErrors

--- src/magicPlace/errors/index.js
+++ src/magicPlace/errors/index.js
@@ -1,4 +1,5 @@
 export const emailExists = { code: 409, msg: 'Email already registered' };
+export const userAlreadyRemoved = { code: 409, msg: 'User already removed' };
 export const emailOrPwdIncorrect = { code: 401, msg: 'Email or password incorrect!' };
 export const tokenIsMissing = { code: 401, msg: 'Token is missing.' };
 export const tokenInvalidOrUnath = {
