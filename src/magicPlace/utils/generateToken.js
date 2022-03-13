de4d6e1587d66cc0f815ef876a1edb9078eec243
AdaoBJr <adaobjr@gmail.com>
2022-03-13 12:11:35 -0400

    feat: :sparkles: added CustomerTokensRepository on generateToken

--- src/magicPlace/utils/generateToken.js
+++ src/magicPlace/utils/generateToken.js
@@ -1,11 +1,18 @@
 import { sign } from 'jsonwebtoken';
 const { JWT_SECRET } = process.env;
+import CustomerTokensRepository from '../../repositories/mongodb/models/customerTokens';
 
 const jwtConfig = {
   algorithm: 'HS256',
   expiresIn: '10m',
 };
 
-const generateToken = ({ id }) => sign({ id }, JWT_SECRET, jwtConfig);
+const generateToken = ({ id }) => {
+  const token = sign({ id }, JWT_SECRET, jwtConfig);
+  const customerToken = { userId: id, token };
+  await CustomerTokensRepository.create(customerToken);
+
+  return token;
+};
 
 export default generateToken;
