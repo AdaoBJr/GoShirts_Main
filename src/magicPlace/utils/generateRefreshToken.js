37c95ead5e0afa4b0cb52cbcd2ff9aa98b90fe26
AdaoBJr <adaobjr@gmail.com>
2022-03-13 16:52:37 -0400

    feat: :boom: added CustomerTokensRepository on generateRefreshToken

--- src/magicPlace/utils/generateRefreshToken.js
+++ src/magicPlace/utils/generateRefreshToken.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { sign } from 'jsonwebtoken';
 import { decodeToken } from '.';
+import CustomerTokensRepository from '../../repositories/mongodb/models/customerTokens';
 import ApiError, { expiredSession } from '../errors';
 
 const { JWT_SECRET } = process.env;
@@ -12,7 +13,7 @@ const jwtConfig = {
 
 const BREAK_POINT = 60;
 
-const generateRefreshToken = ({ token }) => {
+const generateRefreshToken = async ({ token }) => {
   const { id, exp } = decodeToken({ token });
 
   const now = dayjs().format();
@@ -20,8 +21,13 @@ const generateRefreshToken = ({ token }) => {
   const HAS_TIME = expirateDate.diff(now, 'seconds');
 
   if (HAS_TIME > BREAK_POINT) return token;
-  if (BREAK_POINT >= HAS_TIME > 0) return sign({ id }, JWT_SECRET, jwtConfig);
   if (HAS_TIME <= 0) return ApiError(expiredSession);
+  if (BREAK_POINT >= HAS_TIME > 0) {
+    const newToken = sign({ id }, JWT_SECRET, jwtConfig);
+    const data = { userId: id, token: newToken };
+    await CustomerTokensRepository.findOneAndUpdate({ token }, data, { new: true });
+    return newToken;
+  }
 };
 
 export default generateRefreshToken;
