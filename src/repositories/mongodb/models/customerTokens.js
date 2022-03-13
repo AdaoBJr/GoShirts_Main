30c22b7786f31f1fc8bfff126b9490812fb93dd5
AdaoBJr <adaobjr@gmail.com>
2022-03-13 18:34:49 -0400

    feat: :art: updated mongoose schema on CustomerTokens

--- src/repositories/mongodb/models/customerTokens.js
+++ src/repositories/mongodb/models/customerTokens.js
@@ -1,18 +1,15 @@
 import mongoose from 'mongoose';
 
-const Schema = new mongoose.Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-    },
-    token: {
-      type: String,
-      required: true,
-    },
+const Schema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  token: {
+    type: String,
+    required: true,
+  },
+});
 
 const CustomerTokensCollection = 'customerTokens';
 const CustomerTokensRepository = mongoose.model(CustomerTokensCollection, Schema);
