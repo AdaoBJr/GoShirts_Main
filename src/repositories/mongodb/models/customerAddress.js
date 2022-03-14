bfb593df08fa737bb8474d3e8dca2d2246bd7536
AdaoBJr <adaobjr@gmail.com>
2022-03-13 22:34:18 -0400

    feat: :sparkles: created customerAddress on Mongoose

---
+++ src/repositories/mongodb/models/customerAddress.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const Schema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  rua: {
+    type: String,
+    required: true,
+  },
+  bairro: {
+    type: String,
+    required: true,
+  },
+  cidade: {
+    type: String,
+    required: true,
+  },
+  CEP: {
+    type: String,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+  defaultShipping: {
+    type: Boolean,
+    required: true,
+  },
+  defaultBilling: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const CustomerAddressCollection = 'addresses';
+const CustomerAdressRepository = mongoose.model(CustomerAddressCollection, Schema);
+export default CustomerAdressRepository;
