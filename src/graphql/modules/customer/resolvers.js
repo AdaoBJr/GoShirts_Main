1090a983507d6f0288612cfcbe4bd716d3b94a0f
AdaoBJr <adaobjr@gmail.com>
2022-03-16 11:25:13 -0400

    feat: ✨ added createCustomerAddress / CustomerAddress on Resolvers

--- src/graphql/modules/customer/resolvers.js
+++ src/graphql/modules/customer/resolvers.js
@@ -2,17 +2,20 @@ import { useCustomer } from '../../../magicPlace/talons/useCustomer';
 const {
   CustomerList,
   Customer,
+  CustomerAddress,
   CustomerTokens,
   UpdateCustomer,
   DeleteCustomer,
   CreateCustomer,
   SignInCustomer,
   SignOutCustomer,
+  CreateCustomerAddress,
 } = useCustomer();
 
 export default {
   Customer: {
     tokens: async ({ id }) => await CustomerTokens({ id }),
+    address: async ({ id }) => await CustomerAddress({ id }),
   },
   Query: {
     customerList: async () => await CustomerList(),
@@ -20,6 +23,7 @@ export default {
   },
   Mutation: {
     createCustomer: async (_, { data }) => await CreateCustomer({ data }),
+    createCustomerAddress: async (_, args) => await CreateCustomerAddress({ args }),
     updateCustomer: async (_, args) => await UpdateCustomer({ args }),
     deleteCustomer: async (_, { email }) => await DeleteCustomer({ email }),
     signInCustomer: async (_, { data }) => await SignInCustomer({ data }),
