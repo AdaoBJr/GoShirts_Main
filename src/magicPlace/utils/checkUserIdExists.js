7a53eced7e148d04b73018f80f264960317787ea
AdaoBJr <adaobjr@gmail.com>
2022-03-16 11:26:48 -0400

    feat: :sparkles: created checkUserIdExists on utils

---
+++ src/magicPlace/utils/checkUserIdExists.js
@@ -0,0 +1,6 @@
+import CustomerRepository from '../../repositories/mongodb/models/customer';
+
+const checkUserIdExists = async ({ id }) =>
+  await CustomerRepository.findOne({ id }).exec();
+
+export default checkUserIdExists;
