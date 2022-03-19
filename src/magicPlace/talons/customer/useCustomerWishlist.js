0202a01e3c940a80d24ad478079c9deaaec4af3e
AdaoBJr <adaobjr@gmail.com>
2022-03-19 00:25:18 -0400

    feat: ✨ created AddProductsToWishlist on useCustomerWishlist

--- src/magicPlace/talons/customer/useCustomerWishlist.js
+++ src/magicPlace/talons/customer/useCustomerWishlist.js
@@ -1,12 +1,35 @@
-import ApiError, { productExists } from '../../errors';
-import { checkProductExistsByData, generateRefreshToken } from '../../utils';
+import { CustomerWishlistRepository } from '../../../repositories/mongodb/models/customer';
+import ApiError, { unexpectedError, userDoesNotExist } from '../../errors';
+import {
+  checkProductExistsByData,
+  checkUserIdExists,
+  checkWishlistExist,
+  generateRefreshToken,
+} from '../../utils';
 
 const useCustomerWishlist = () => {
-  const AddProductsToWishlist = async ({ args: { id, token, data } }) => {
-    const checkedProducts = await checkProductExistsByData({ data });
-    console.log('CHECKED', checkedProducts);
-    // if (!product) ApiError(productExists);
-    return { token: generateRefreshToken({ token }), wishlist: data };
+  const AddProductsToWishlist = async ({ args: { id: userId, token, data } }) => {
+    const user = await checkUserIdExists({ userId });
+    if (!user) ApiError(userDoesNotExist);
+
+    const wishlistExists = await checkWishlistExist({ userId });
+
+    const customerWishlist = { userId, wishlist: data };
+
+    if (wishlistExists)
+      await CustomerWishlistRepository.findOneAndUpdate({ userId }, customerWishlist, {
+        new: true,
+      });
+
+    if (!wishlistExists) {
+      const created = await CustomerWishlistRepository.create(customerWishlist);
+      if (!created) ApiError(unexpectedError);
+    }
+
+    return {
+      token: generateRefreshToken({ token }),
+      wishlist: await checkProductExistsByData({ data }),
+    };
   };
 
   return { AddProductsToWishlist };
