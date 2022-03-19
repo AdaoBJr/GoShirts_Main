49b486b48ade745b24c0caab3c241d5bd1015be5
AdaoBJr <adaobjr@gmail.com>
2022-03-19 17:51:44 -0400

    feat: :zap: updated checkProductExistsByData on util

--- src/magicPlace/utils/checkProductExistsByData.js
+++ src/magicPlace/utils/checkProductExistsByData.js
@@ -2,15 +2,18 @@ import { ProductRepository } from '../../repositories/mongodb/models/products';
 import { productDoesNotExists as error } from '../errors';
 
 const checkProductExistsByData = async ({ data }) => {
-  const checked = await Promise.all(
+  const wishChecked = await Promise.all(
     data.map(async ({ sku, quantity }) => ({
       sku,
       quantity,
       error: !(await ProductRepository.findOne({ sku }).exec()) && error.msg,
     }))
   );
-  for (const i in checked) if (checked[i].error !== error.msg) delete checked[i].error;
-  return checked;
+  for (const i in wishChecked)
+    if (wishChecked[i].error !== error.msg) delete wishChecked[i].error;
+  const wishData = wishChecked.filter((item) => !item.error);
+
+  return { wishChecked, wishData };
 };
 
 export default checkProductExistsByData;
