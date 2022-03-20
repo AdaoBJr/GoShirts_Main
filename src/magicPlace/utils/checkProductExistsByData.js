1b2f2b75236313fa000b94669206c4e9b663ca37
AdaoBJr <adaobjr@gmail.com>
2022-03-20 09:57:33 -0400

    feat: :boom: improve habilities func checkProducExistsByData on utils

--- src/magicPlace/utils/checkProductExistsByData.js
+++ src/magicPlace/utils/checkProductExistsByData.js
@@ -1,13 +1,22 @@
+import { CustomerWishlistRepository } from '../../repositories/mongodb/models/customer';
 import { ProductRepository } from '../../repositories/mongodb/models/products';
 import { productDoesNotExists as error } from '../errors';
 
-const checkProductExistsByData = async ({ data }) => {
+const checkProductExistsByData = async ({ userId, data, decrease }) => {
   const wishChecked = await Promise.all(
-    data.map(async ({ sku, quantity }) => ({
-      sku,
-      quantity,
-      error: !(await ProductRepository.findOne({ sku }).exec()) && error.msg,
-    }))
+    data.map(async ({ sku, quantity }) => {
+      const prodRepo = await ProductRepository.findOne({ sku }).exec();
+      const wishRepo = decrease
+        ? await CustomerWishlistRepository.findOne({ userId }).exec()
+        : !decrease;
+      const prodInWish = wishRepo && wishRepo.wishlist.find((item) => item.sku === sku);
+
+      return {
+        sku,
+        quantity,
+        error: (!prodRepo || !prodInWish) && error.msg,
+      };
+    })
   );
   for (const i in wishChecked)
     if (wishChecked[i].error !== error.msg) delete wishChecked[i].error;
