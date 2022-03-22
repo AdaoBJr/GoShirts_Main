8d736b8aff12429e96497041196c9b6b488226db
AdaoBJr <adaobjr@gmail.com>
2022-03-21 22:46:42 -0400

    feat: ✨ created generateTokenResetEmail

---
+++ src/magicPlace/utils/generateTokenResetEmail.js
@@ -0,0 +1,19 @@
+import { sign } from 'jsonwebtoken';
+const { JWT_SECRET } = process.env;
+import { CustomerTokensRepository } from '../../repositories/mongodb/models/customer';
+
+const jwtConfig = {
+  algorithm: 'HS256',
+  expiresIn: '3h',
+};
+
+const generateTokenResetEmail = async ({ id: userId }) => {
+  await CustomerTokensRepository.deleteMany({ userId });
+  const token = sign({ id: userId }, JWT_SECRET, jwtConfig);
+  const customerToken = { userId, token };
+  await CustomerTokensRepository.create(customerToken);
+
+  return token;
+};
+
+export default generateTokenResetEmail;
--- src/magicPlace/utils/index.js
+++ src/magicPlace/utils/index.js
@@ -1,6 +1,8 @@
 import decodeToken from './decodeToken';
 import generateToken from './generateToken';
+import requestAPIGateway from './requestAPIGateway';
 import generateRefreshToken from './generateRefreshToken';
+import generateTokenResetEmail from './generateTokenResetEmail';
 import checkEmailExists from './checkEmailExists';
 import checkTokenExists from './checkTokenExists';
 import checkUserIdExists from './checkUserIdExists';
@@ -14,7 +16,9 @@ import decreaseProductWishlist from './decreaseProductWishlist';
 export {
   decodeToken,
   generateToken,
+  requestAPIGateway,
   generateRefreshToken,
+  generateTokenResetEmail,
   checkEmailExists,
   checkTokenExists,
   checkUserIdExists,
