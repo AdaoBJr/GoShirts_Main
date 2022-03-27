b258baded38e7ff91b221610034f531b20f53ce2
AdaoBJr <adaobjr@gmail.com>
2022-03-27 17:40:02 -0400

    refactor: :recycle: updated export Errors

--- src/magicPlace/errors/index.js
+++ src/magicPlace/errors/index.js
@@ -1,21 +1,16 @@
-export const unexpectedError = { code: 500, msg: 'Unexpected Error.' };
-export const emailExists = { code: 409, msg: 'Email already registered.' };
-export const userAlreadyRemoved = { code: 409, msg: 'User already removed.' };
-export const emailOrPwdIncorrect = { code: 401, msg: 'Email or password incorrect!' };
-export const tokenIsMissing = { code: 401, msg: 'Token is missing.' };
-export const tokenInvalidOrUnath = {
-  code: 401,
-  msg: 'Invalid token or unauthorized user.',
-};
-export const expiredSession = { code: 401, msg: 'Expired session.' };
-export const userDoesNotExist = { code: 401, msg: 'User does not exists.' };
-export const addressDoesNotExist = { code: 401, msg: 'Address does not exists.' };
-export const productExists = { code: 409, msg: 'Product already registered.' };
-export const productDoesNotExists = { code: 401, msg: 'Product does not exists.' };
-export const wishlistDoesNotExistsOrIsEmpty = {
-  code: 401,
-  msg: 'Wishlist does not exists or is empty.',
-};
-export const dataIsMissing = { code: 401, msg: 'Data is missing.' };
-
 export { default } from './ApiError';
+export {
+  unexpectedError,
+  emailExists,
+  userAlreadyRemoved,
+  emailOrPwdIncorrect,
+  tokenIsMissing,
+  tokenInvalidOrUnath,
+  expiredSession,
+  userDoesNotExist,
+  addressDoesNotExist,
+  productExists,
+  productDoesNotExists,
+  wishlistDoesNotExistsOrIsEmpty,
+  dataIsMissing,
+} from './msgError';
